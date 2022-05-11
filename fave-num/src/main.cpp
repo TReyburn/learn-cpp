3ac2ae0ae62cf6870b2315ac0089dc350ed02a06
Travis Reyburn <treyburn@alteryx.com>
2022-05-11 14:04:04 -0600

    added a simple cli app

---
+++ fave-num/src/main.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+
+int main() {
+    int num;
+
+    std::cout << "what's your favorite number?" << std::endl;
+    std::cout << "enter between 1 and 100: ";
+
+    std::cin >> num;
+    if (num < 1 || num > 100) {
+        std::cout << num << " is not a valid number. only between 1 and 100" << std::endl;
+        return 1;
+    }
+
+    std::cout << "oh wow - " << num << " is my favorite number too" << std::endl;
+    return 0;
+}
