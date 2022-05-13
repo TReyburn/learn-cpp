e38c7c359a4bb31900320fc563deaeeb9b42bbab
Travis Reyburn <treyburn@alteryx.com>
2022-05-13 09:38:01 -0600

    tidy

--- fave-num/src/main.cpp
+++ fave-num/src/main.cpp
@@ -5,8 +5,8 @@ int main() {
 
     std::cout << "what's your favorite number?" << std::endl;
     std::cout << "enter between 1 and 100: ";
-
     std::cin >> num;
+
     if (num < 1 || num > 100) {
         std::cout << num << " is not a valid number. only between 1 and 100" << std::endl;
         return 1;
