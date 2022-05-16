be5e0edd7c5b767f21a926463cc6bf4371a57f7e
Travis Reyburn <treyburn@alteryx.com>
2022-05-16 12:26:10 -0600

    square footage calc

---
+++ sqft/src/main.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+  int roomWidth{ 0 };
+  int roomLength{ 0 };
+
+  cout << "Enter the width of the room: ";
+  cin >> roomWidth;
+
+  cout << "Enter the length of the room: ";
+  cin >> roomLength;
+
+  cout << "Square footage: " << (roomWidth * roomLength);
+
+  return 0;
+}
